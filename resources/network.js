be215c1ecf4936a1beaf494e2f80732ad6c11c1b
Szymon Klarman <sklarman@users.noreply.github.com>
2026-01-27 08:26:55 +0100

    Rename 'Google Knowledge Graph' to 'Google Knowledge Panel'

--- resources/network.js
+++ resources/network.js
@@ -10,7 +10,7 @@ var nodes = new vis.DataSet([
         {id: 10, title: 'SlideShare', image: 'resources/slideshare.png', shape: 'image', url: 'http://slideshare.net/SzymonKlarman'},
         {id: 11, title: 'Medium', image: 'resources/medium.png', shape: 'image', url: 'http://medium.com/@sklarman'},
         {id: 12, title: 'Google AI Search', image: 'resources/google-ai-search.png', shape: 'image', url: 'https://www.google.com/search?q=Who+is+Szymon+Klarman%3F&udm=50'},
-        {id: 13, title: 'Google Knowledge Graph', image: 'resources/google-search.png', shape: 'image', url: 'https://www.google.com/search?kgmid=/g/11f0zc5209'}
+        {id: 13, title: 'Google Knowledge Panel', image: 'resources/google-search.png', shape: 'image', url: 'https://www.google.com/search?kgmid=/g/11f0zc5209'}
    ]);
 
 //    {id: 7, title: 'CV', image: 'resources/cv.png', shape: 'image', url: 'resources/Klarman_CV.pdf'},
